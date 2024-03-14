e06f1d2549ec8dc38c89176a62b6c386a60b1299
Sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-03-15 00:25:17 +0530

    added styles

--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -13,7 +13,7 @@ export default function Resumepreview({
   return (
     <div className="resume-preview-container">
       <div className="resume-content">
-        <div className="resume-section">
+        <div className="personal-info">
           <div className="name">
             <h2 id="resume-name">{name}</h2>
             <p className="info">
@@ -36,31 +36,69 @@ export default function Resumepreview({
           </div>
         </div>
         <div className="resume-section">
-          {education.length > 0 && <h2 className="edu-prev">Education </h2>}
+          {education.length > 0 && <h3 className="header-text">Education </h3>}
           {education.map((educationEntry, index) => (
             <div key={index} className="education-entry">
-              <p className="institution">{educationEntry.institution}</p>
-              <p className="degree">{educationEntry.degree}</p>
-              <p className="date">
-                {educationEntry.startDate}{" "}
-                {educationEntry.endDate && ` - ${educationEntry.endDate}`}
-              </p>
-              <p className="location">{educationEntry.location}</p>
+              <div className="edu-info-group">
+                <p className="date">
+                  {educationEntry.startDate}{" "}
+                  {educationEntry.endDate && ` - ${educationEntry.endDate}`}
+                  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                  <p className="location">{educationEntry.location}</p>
+                </p>
+              </div>
+              <div className="edu-info-group">
+                <p className="edu-info-schoolname">
+                  {educationEntry.institution}
+                </p>
+                <p className="edu-info-degree">{educationEntry.degree}</p>
+              </div>
             </div>
           ))}
         </div>
         <div className="resume-section">
-          {work.length > 0 && <h2 className="exp-prev">Work Experience</h2>}
+          {work.length > 0 && <h3 className="header-text">Work Experience</h3>}
+          {/* {work.map((workEntry, index) => (
+            <div key={index} className="experience-info">
+              <div className="experience-info-group">
+                <p className="date">
+                  {workEntry.startDate}{" "}
+                  {workEntry.endDate && ` - ${workEntry.endDate}`}
+                  <p className="location">{workEntry.location}</p>
+                </p>
+              </div>
+              <div className="experience-info-group">
+                <p className="experience-info-companyname">
+                  {workEntry.company}
+                </p>
+                <p className="experience-info-position">{workEntry.position}</p>
+                <div className="desc">
+                  <p className="experience-info-description">
+                    {workEntry.description}
+                  </p>
+                </div>
+              </div>
+            </div> */}
           {work.map((workEntry, index) => (
-            <div key={index} className="work-entry">
-              <p className="company">{workEntry.company}</p>
-              <p className="position">{workEntry.position}</p>
-              <p className="date">
-                {workEntry.startDate}{" "}
-                {workEntry.endDate && ` - ${workEntry.endDate}`}
-              </p>
-              <p className="location">{workEntry.location}</p>
-              <p className="description">{workEntry.description}</p>
+            <div key={index} className="experience-info">
+              <div className="date-location">
+                <p className="date">
+                  {workEntry.startDate}{" "}
+                  {workEntry.endDate && ` - ${workEntry.endDate}`}
+                </p>
+                <p className="location">{workEntry.location}</p>
+              </div>
+              <div className="company-description">
+                <p className="experience-info-companyname">
+                  {workEntry.company}
+                </p>
+                <p className="experience-info-position">{workEntry.position}</p>
+                <div className="desc">
+                  <p className="experience-info-description">
+                    {workEntry.description}
+                  </p>
+                </div>
+              </div>
             </div>
           ))}
         </div>
@@ -79,7 +117,6 @@ Resumepreview.propTypes = {
 };
 
 /*
-Button Styles
 Resume Preview style
 Fix heights
 Align form
